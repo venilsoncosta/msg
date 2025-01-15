a2ca2aa695c5b6d16a6319e7e867d9c5e5642a34
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2025-01-15 06:40:20 -0300

    cirado o projeto

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('meuBotao').addEventListener('click', function() {
+    var mensagem = document.getElementById('mensagem');
+    var texto = 'Bom Dia! Essa mensagem é para reforçar que sim, você é capaz de realizar tudo o que deseja. Acredite em você!';
+    mensagem.innerHTML = '';
+    mensagem.style.opacity = 1;
+    mensagem.style.transform = 'translateY(0)';
+
+    var i = 0;
+    var intervalo = setInterval(function() {
+        if (i < texto.length) {
+            mensagem.innerHTML += texto.charAt(i);
+            i++;
+        } else {
+            clearInterval(intervalo);
+        }
+    }, 200); // Intervalo de 200ms entre cada letra
+});
